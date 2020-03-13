dd5baed442dfc837271d0ea920b10f123403b897
Alex Loomis <atloomis@math.arizona.edu>
2020-03-12 21:37:29 -0700

    Updated test suite.

--- test/Test.hs
+++ test/Test.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE TypeOperators #-}
 
+import Control.Monad.Random (mkStdGen)
 import Data.Ratio
 import Test.Framework
 
@@ -10,47 +11,46 @@ import Markov.Extra
 
 main = htfMain htf_thisModulesTests
 
--- Examples in the documentation.
 test_fromLists =
     assertEqual
-    (chain [pure (FromLists 't') :: (Product Double, FromLists)] !! 100)
     [ (0.5060975609756099, FromLists 'a')
     , (0.29268292682926833, FromLists 'l')
     , (0.201219512195122, FromLists 't') ]
+    (chain [pure (FromLists 't') :: (Product Double, FromLists)] !! 100)
 
 test_m0Simple =
     assertEqual
-    (take 3 $ chain0 [Simple 0])
     [ [0]
     , [-1,1]
     , [-2,0,2] ]
+    (take 3 $ chain0 [Simple 0])
 
 test_pdSimple =
     assertEqual
-    (take 3 $ chain [pure 0 :: (Product Double, Simple)])
     [ [(1.0,0)]
     , [(0.5,-1),(0.5,1)]
     , [(0.25,-2),(0.5,0),(0.25,2)] ]
+    (take 3 $ chain [pure 0 :: (Product Double, Simple)])
 
 test_piSimple =
     assertEqual
-    (take 3 $ chain [pure 0 :: (Product Int, Simple)])
     [ [(1,0)]
     , [(1,-1),(1,1)]
     , [(1,-2),(2,0),(1,2)] ]
+    (take 3 $ chain [pure 0 :: (Product Int, Simple)])
 
 test_siSimple =
     assertEqual
-    (chain [pure 0 :: (Sum Int, Simple)] !! 2)
     [ (0,0), (0,1), (0,2), (1,-1), (1,0), (2,-2) ]
+    (chain [pure 0 :: (Sum Int, Simple)] !! 2)
 
 test_HMM =
     assertEqual
-    (filter (\((_,Merge xs),_) -> xs == "aaa") $ chain
-    [1 >*< Merge "" >*< 1 :: Product Rational :* Merge String :* Room] !! 3)
     [ ((Product $ 3243 % 200000, Merge "aaa"),Room 1)
     , ((Product $ 9729 % 500000, Merge "aaa"),Room 2)
     , ((Product $ 4501 % 250000, Merge "aaa"),Room 3) ]
+    (filter (\((_,Merge xs),_) -> xs == "aaa") $ chain
+    [1 >*< Merge "" >*< 1 :: Product Rational :* Merge String :* Room] !! 3)
 
 test_expLoss =
     assertEqual
@@ -59,5 +59,11 @@ test_expLoss =
 
 test_expLossMore =
     assertEqual
-    (expectedLoss [pure $ initial [1,0,3,2,2,5] :: (Product Double, FillBin)])
     9.764425505050506
+    (expectedLoss [pure $ initial [1,0,3,2,2,5] :: (Product Double, FillBin)])
+
+test_randomPath =
+    assertEqual
+    (0.1648351648351649, Urn (2,13))
+    (randomPath (mkStdGen 70) (Urn (2,5)) !! 8 :: (Product Double, Urn))
+
